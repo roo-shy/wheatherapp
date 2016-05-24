f62de846f15e04cb11f804924c130b8f172e765c
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-24 00:07:39 -0500

    Fixing routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -8,11 +8,11 @@ var Main = require('../containers/Main');
 var Home = require('../components/Home');
 
 var routes = (
-  <Router hashistory>
+  <Router history={hashHistory}>
    <Route path='/' component={Main}>
-   <IndexRoute component={Home}>
+   <IndexRoute component={Home} />
    </Route>
   </Router>
-);
+)
 
 module.exports = routes;
