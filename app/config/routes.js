6e24465ccde67607a41564b74854d9ee247221c2
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-27 15:14:07 -0500

    Adding styiling to index.html, new routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,16 +2,19 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var Route = ReactRouter.Route;
-var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
+var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../containers/Main');
 var HomeContainer = require('../containers/HomeContainer');
+var ForecastContainer = require('../containers/ForecastContainer');
+var DetailContainer = require('../containers/DetailContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-    <IndexRoute component={HomeContainer} />
+      <IndexRoute component={HomeContainer} />
+      <Route path='forecast/:city' component={ForecastContainer} />
+      <Route path='detail/:city' component={DetailContainer} />
     </Route>
   </Router>
 );
--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,10 +1,18 @@
 var React = require('react');
-var Header = require('../components/Header')
+var GetCityContainer = require('./GetCityContainer');
 
 var styles = {
   container: {
     width: '100%',
-    height: '94%'
+    height: '92%'
+  },
+  header :{
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: 'rgba(252, 90, 44, 0.89)',
+    color: '#fff',
+    padding: 5,
   }
 }
 
@@ -12,11 +20,14 @@ var Main = React.createClass({
   render: function () {
     return (
       <div style={styles.container}>
-        <Header />
+        <div style={styles.header}>
+          <h2 style={{margin: 0}}>Clever Title</h2>
+          <GetCityContainer direction='row' />
+        </div>
         {this.props.children}
       </div>
     )
   }
-});
+})
 
 module.exports = Main;
